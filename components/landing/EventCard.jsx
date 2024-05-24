38d6e4e8d49ea2e8c785eedd0d569b105863260b
Usuf Ali <usuf.iu.cse16@gmail.com>
2024-05-24 18:09:26 +0600

    mongoose, event and event details added

--- components/landing/EventCard.jsx
+++ components/landing/EventCard.jsx
@@ -1,24 +1,33 @@
-import GoogleIO from "@/public/google-io-2023-1.png";
 import Image from "next/image";
 import Link from "next/link";
 import ActionsButtons from "../ActionsButtons";
 
-const EventCard = () => {
+const EventCard = ({ event }) => {
   return (
     <div className='overflow-hidden rounded-md bg-[#242526]'>
-      <Image src={GoogleIO} alt='Event 1' className='w-full' />
+      <div className='w-[500px] h-[220px] overflow-hidden'>
+        <Image
+          src={event?.imageUrl}
+          alt={event?.name}
+          width={500}
+          height={500}
+          className='w-full object-cover'
+        />
+      </div>
 
       <div className='p-3'>
-        <Link href='/details/1' className='font-bold text-lg'>
-          Google I/O Extended
+        <Link href={`/details/${event.id}`} className='font-bold text-lg'>
+          {event?.name}
         </Link>
-        <p className='text-[#9C9C9C] text-sm mt-1'>
-          Rangpur, Dhaka, Bangladesh, Rangpur, Bangladesh
+        <p className='text-[#9C9C9C] text-sm mt-1 h-16 overflow-hidden'>
+          {event?.details.length < 115
+            ? event.details
+            : event.details.substring(0, 115)}
         </p>
         <div className='text-[#737373] text-sm mt-1'>
-          <span>1k Interested</span>
+          <span>{event?.interested_ids?.length}k Interested</span>
           <span>|</span>
-          <span>40K Going</span>
+          <span>{event?.going_ids?.length}K Going</span>
         </div>
 
         <ActionsButtons />
