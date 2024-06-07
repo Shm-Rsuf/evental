ad63a94002e8eb3caa19b85df8653310f978d375
Usuf Ali <usuf.iu.cse16@gmail.com>
2024-06-07 14:29:13 +0600

    search query's issue solved

--- components/landing/Search.jsx
+++ components/landing/Search.jsx
@@ -1,7 +1,25 @@
+"use client";
+
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
 const Search = () => {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+  /* handleChang */
+  const handleChang = (text) => {
+    const params = new URLSearchParams(searchParams);
+    if (text) {
+      params.set("query", text);
+    } else {
+      params.delete("query");
+    }
+    replace(`${pathname}?${params.toString()}`);
+  };
   return (
     <div>
       <input
+        onChange={(e) => handleChang(e.target.value)}
         type='text'
         placeholder='Search...'
         className='bg-[#27292F] border border-[#CCCCCC]/20 py-1 px-2 rounded-md focus:outline-none focus:border-cyan-200/50'
