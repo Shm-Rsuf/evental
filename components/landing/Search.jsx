35b6e6e44f1011124af9ffa769255ef9b0a09e76
Usuf Ali <usuf.iu.cse16@gmail.com>
2024-06-08 20:33:16 +0600

    useDebounce hook added

--- components/landing/Search.jsx
+++ components/landing/Search.jsx
@@ -1,13 +1,14 @@
 "use client";
 
+import useDebounce from "@/app/hooks/useDebounce";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 const Search = () => {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
-  /* handleChang */
-  const handleChang = (text) => {
+
+  const doSearch = useDebounce((text) => {
     const params = new URLSearchParams(searchParams);
     if (text) {
       params.set("query", text);
@@ -15,11 +16,17 @@ const Search = () => {
       params.delete("query");
     }
     replace(`${pathname}?${params.toString()}`);
+  }, 500);
+  /* handleChang */
+  const handleChang = (text) => {
+    doSearch(text);
   };
+
   return (
     <div>
       <input
         onChange={(e) => handleChang(e.target.value)}
+        defaultValue={searchParams.get("query")?.toString()}
         type='text'
         placeholder='Search...'
         className='bg-[#27292F] border border-[#CCCCCC]/20 py-1 px-2 rounded-md focus:outline-none focus:border-cyan-200/50'
